ffe2ec7ec197aaf8319f3b8c10fa320197ed1c5a
farhatuloffical78 <mdfarhatulhaider@gmail.com>
2024-10-13 23:57:00 +0600

    updating modu tasks

---
+++ Tasks of Conditionals/freeDrinks.js
@@ -0,0 +1,17 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+x= 600;
+let billAmount = x;
+if (x > 500) {
+    console.log('Enjoy your Free Coke!!');
+
+}
+else{
+    console.log("Coke Price: 30taka");
+}
+
