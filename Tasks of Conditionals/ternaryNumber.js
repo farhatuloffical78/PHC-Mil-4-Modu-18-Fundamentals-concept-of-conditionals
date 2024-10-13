0a178f54fcce5ec3487d1a1bbbd577f4a98c583d
farhatuloffical78 <mdfarhatulhaider@gmail.com>
2024-10-14 00:09:30 +0600

    updating modu task5

---
+++ Tasks of Conditionals/ternaryNumber.js
@@ -0,0 +1,28 @@
+// Task: 5
+
+/***
+
+you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. 
+
+also, write it using ternary operator.
+
+ */
+
+let num1 = 1000, num2=500, result;
+
+// By using ternary
+
+// if (num1 > num2) {
+//     result = num1*2;
+// }
+// else {
+//     result = (num1 + num2)/ 2;
+// }
+// console.log(result);
+
+(num1 > num2) ? console.log(result = num1*2) : console.log(result = (num1 + num2)/2) ;
